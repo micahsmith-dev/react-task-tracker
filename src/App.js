f55a2ff2b17d7e6cfbc5e0d46137e675206f92a4
DoggyLicious <dogrgdloveryt11@gmail.com>
2021-04-17 22:13:17 -0400

    add build for production

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Doctor\'s Appointment',
-        day: 'Feb 5th, 2:30pm',
-        reminder: true,
+      id: 1,
+      text: 'Doctor\'s Appointment',
+      day: 'Feb 5th, 2:30pm',
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'School Meeting',
-        day: 'Feb 6th, 1:30pm',
-        reminder: true,
+      id: 2,
+      text: 'School Meeting',
+      day: 'Feb 6th, 1:30pm',
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Grocery Shopping',
-        day: 'Feb 5th, 2:30pm',
-        reminder: false,
+      id: 3,
+      text: 'Grocery Shopping',
+      day: 'Feb 5th, 2:30pm',
+      reminder: false,
     },
-])
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+  
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
 
   return (
-    <div className="container">
-      <Header />
-      <Tasks tasks={tasks} />
+    <div className='container'>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks are Scheduled for Today!'
+      )}
     </div>
   )
 }
